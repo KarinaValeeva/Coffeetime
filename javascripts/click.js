9b36a73220f5fee278e9e770dc71f1cf3f4378e0
Karina <Karina@MacBook-Pro-Karina.local>
2017-02-28 00:48:32 +0300

    добавила проект
    Поменяла всю структуру проекта
    Было интересно поработать со всплывающими окнами

---
+++ javascripts/click.js
@@ -0,0 +1,133 @@
+// $(function() {
+//   $('.banner').click(function() {
+//     $('.banner').attr('class', 'fill');
+//   });
+// })
+
+  // $(".cup").click(function () {
+  //   $(".cup figure:hidden:last").fadeIn("Down")
+  // })
+//
+// $(function() {
+//   $('.cup').click(function () {
+//     console.log('clicked')
+//     $(this).find('.fill').addClass('active')
+//   })
+// })
+$(function() {
+  $(".modal-content .button1").click(function () {
+    $(".cup .first").fadeIn("Up")
+  })
+
+  $(".modal-content .button2").click(function () {
+    $(".cup .text1").fadeIn("Up")
+  })
+})
+
+$(function() {
+  $(".modal-content1 .button1").click(function () {
+    $(".cup .second").fadeIn("Up")
+  })
+
+  $(".modal-content1 .button2").click(function () {
+    $(".cup .text2, .text3").fadeIn("Up")
+  })
+})
+
+$(function() {
+  $(".modal-content2 .button1").click(function () {
+    $(".cup1 .third").fadeIn("Up")
+  })
+
+  $(".modal-content2 .button2").click(function () {
+    $(".cup1 .text4, .text5, .text6").fadeIn("Up")
+  })
+})
+
+$(function() {
+  $(".modal-content3 .button1").click(function () {
+    $(".cup1 .fourth").fadeIn("Up")
+  })
+
+  $(".modal-content3 .button2").click(function () {
+    $(".cup1 .text7, .text8").fadeIn("Up")
+  })
+})
+
+$(function() {
+  $(".modal-content4 .button1").click(function () {
+    $(".cup1 .fifth").fadeIn("Up")
+  })
+
+  $(".modal-content4 .button2").click(function () {
+    $(".cup1 .text9, .text10, .text11").fadeIn("Up")
+  })
+})
+
+$(function() {
+  $(".modal-content5 .button1").click(function () {
+    $(".cup1 .six").fadeIn("Up")
+  })
+
+  $(".modal-content5 .button2").click(function () {
+    $(".cup1 .text12, .text13, .text14").fadeIn("Up")
+  })
+})
+
+// $(function() {
+//   $('.cup').click(function () {
+//     console.log('clicked')
+//     $(this).find('.first').addClass('active')
+//   })
+// })
+
+//
+// $(function() {
+//   $(".cup1").click(function () {
+//     $(".cup1 figure:hidden:last").fadeIn("Down")
+//   })
+// })
+//
+//
+// $(function() {
+//   $(".cup2").click(function () {
+//     $(".cup2 figure:hidden:last").fadeIn("Down")
+//   })
+// })
+//
+//
+// $(function() {
+//   $(".cup3").click(function () {
+//     $(".cup3 figure:hidden:last").fadeIn("Down")
+//   })
+// })
+//
+// $(function() {
+//   $(".cup4").click(function () {
+//     $(".cup4 figure:hidden:last").fadeIn("Down")
+//   })
+// })
+//
+// $(function() {
+//   $(".cup5").click(function () {
+//     $(".cup5 figure:hidden:last").fadeIn("Down")
+//   })
+// })
+//
+// $(function() {
+//   $(".cup6").click(function () {
+//     $(".cup6 figure:hidden:last").fadeIn("Down")
+//   })
+// })
+//
+// $(function() {
+//   $(".cup7").click(function () {
+//     $(".cup7 figure:hidden:last").fadeIn("Down")
+//   })
+// })
+//
+// $(function() {
+//   $(".cup8").click(function () {
+//     $(".cup8 figure:hidden:last").fadeIn("Down")
+//   })
+// })
