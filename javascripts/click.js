47c62e1de3c79fc1bc7f10f29551062a7b91c08f
Karina <Karina@MacBook-Pro-Karina.local>
2017-03-01 04:05:50 +0300

    Небольшие поправки

--- javascripts/click.js
+++ javascripts/click.js
@@ -1,134 +1,65 @@
-// $(function() {
-//   $('.banner').click(function() {
-//     $('.banner').attr('class', 'fill');
-//   });
-// })
+$(function() {
+$("#espresso").animatedModal();
+ $("#cappuchino").animatedModal({
+      modalTarget:'animatedModal2',
+      color:'#fff',
+  });
+  $("#macchiato").animatedModal({
+       modalTarget:'animatedModal1',
+       color:'#fff',
+   });
+   $("#americano").animatedModal({
+        modalTarget:'animatedModal3',
+        color:'#fff',
+    });
+    $("#mocha").animatedModal({
+         modalTarget:'animatedModal4',
+         color:'#fff',
+     });
+     $("#latte").animatedModal({
+          modalTarget:'animatedModal5',
+          color:'#fff',
+      });
 
-  // $(".cup").click(function () {
-  //   $(".cup figure:hidden:last").fadeIn("Down")
-  // })
-//
-// $(function() {
-//   $('.modal-content1 .button1').click(function () {
-//     console.log('clicked')
-//     $(this).find('.first').fadeIn("Up")
-//   })
-// })
+      $("#breve").animatedModal({
+           modalTarget:'animatedModal6',
+           color:'#fff',
+       });
+
+       $("#raf").animatedModal({
+            modalTarget:'animatedModal7',
+            color:'#fff',
+        });
+
+        $("#glasse").animatedModal({
+             modalTarget:'animatedModal8',
+             color:'#fff',
+         });
+     })
+
 
 $(function() {
   $(".modal-content .button1").click(function () {
     $(".cup .first").show(".first")
   })
 
-  $(".modal-content .button2").click(function () {
-    $(".cup .text1").show(".text1")
-  })
-})
-
-$(function() {
   $(".modal-content1 .button1").click(function () {
     $(".cup .second").show(".second")
   })
 
-  $(".modal-content1 .button2").click(function () {
-    $(".cup .text2, .text3").show(".text2, .text3")
-  })
-})
-
-$(function() {
   $(".modal-content2 .button1").click(function () {
     $(".cup1 .third").show(".third")
   })
 
-  $(".modal-content2 .button2").click(function () {
-    $(".cup1 .text4, .text5, .text6").show(".text4, .text5, .text6")
-  })
-})
-
-$(function() {
   $(".modal-content3 .button1").click(function () {
     $(".cup1 .fourth").show(".fourth")
   })
 
-  $(".modal-content3 .button2").click(function () {
-    $(".cup1 .text7, .text8").show(".text7, .text8")
-  })
-})
-
-$(function() {
   $(".modal-content4 .button1").click(function () {
     $(".cup1 .fifth").show(".fifth")
   })
 
-  $(".modal-content4 .button2").click(function () {
-    $(".cup1 .text9, .text10, .text11").show(".text9, .text10, .text11")
-  })
-})
-
-$(function() {
   $(".modal-content5 .button1").click(function () {
     $(".cup1 .six").show(".six")
   })
-
-  $(".modal-content5 .button2").click(function () {
-    $(".cup1 .text12, .text13, .text14").show(".text12, .text13, .text14")
-  })
 })
-
-// $(function() {
-//   $('.cup').click(function () {
-//     console.log('clicked')
-//     $(this).find('.first').addClass('active')
-//   })
-// })
-
-//
-// $(function() {
-//   $(".cup1").click(function () {
-//     $(".cup1 figure:hidden:last").fadeIn("Down")
-//   })
-// })
-//
-//
-// $(function() {
-//   $(".cup2").click(function () {
-//     $(".cup2 figure:hidden:last").fadeIn("Down")
-//   })
-// })
-//
-//
-// $(function() {
-//   $(".cup3").click(function () {
-//     $(".cup3 figure:hidden:last").fadeIn("Down")
-//   })
-// })
-//
-// $(function() {
-//   $(".cup4").click(function () {
-//     $(".cup4 figure:hidden:last").fadeIn("Down")
-//   })
-// })
-//
-// $(function() {
-//   $(".cup5").click(function () {
-//     $(".cup5 figure:hidden:last").fadeIn("Down")
-//   })
-// })
-//
-// $(function() {
-//   $(".cup6").click(function () {
-//     $(".cup6 figure:hidden:last").fadeIn("Down")
-//   })
-// })
-//
-// $(function() {
-//   $(".cup7").click(function () {
-//     $(".cup7 figure:hidden:last").fadeIn("Down")
-//   })
-// })
-//
-// $(function() {
-//   $(".cup8").click(function () {
-//     $(".cup8 figure:hidden:last").fadeIn("Down")
-//   })
-// })
